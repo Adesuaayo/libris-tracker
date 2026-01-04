d4c9f3ec5c5c4d41d66415d685d64b49eb6515f7
Adesua Ayomitan <adesuadonatus45@gmail.com>
2026-01-04 21:35:58 +0100

    Stop tracking config.ts - contains API keys

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-// Auto-generated at build time - DO NOT EDIT
-export const ENV_CONFIG = {
-  VITE_GEMINI_API_KEY: 'AIzaSyDvbGTHaCEAlgSb4_Zl-r2Co6twR7dL-BE',
-  VITE_SUPABASE_URL: 'https://rdxbmifcclfbdfgvnikg.supabase.co',
-  VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkeGJtaWZjY2xmYmRmZ3ZuaWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1MjQ3MDQsImV4cCI6MjA4MDEwMDcwNH0.rQAepf1hrLatGlRHxstsQCd4ZHXWqxqxnFHS35Sxmd4',
-};
